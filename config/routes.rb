5aad220708cd798c671ff5b8083663338b456626
Sami Hindi <sami.hindi@renuo.ch>
2024-08-14 15:52:23 +0200

    feat: add basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'dashboard#index'
+  get 'profile', to: 'profile#show'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
